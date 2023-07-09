c77887976a89bd5fb1b6e5c15a902db0e9d69759
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-07-09 18:35:03 +0700

    fix: update logo width

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-white leading-[1.6]" :style="{ fontFamily: `'IBM Plex Sans Thai Looped', sans-serif` }">
     <div class="px-4 py-2">
-      <AppLogo class="w-[7em] h-auto mx-auto" />
+      <AppLogo class="w-[7em] md:w-[10em] h-auto mx-auto" />
     </div>
     <div class="container mx-auto px-6 pb-6 max-w-3xl">
       <slot />
