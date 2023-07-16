cb159207b854f61719752060fffbd795bedc1783
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-07-16 23:40:05 +0700

    feat: implement BASE_URL

--- server/plugins/og.ts
+++ server/plugins/og.ts
@@ -4,7 +4,7 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:afterParse', (file) => {
     if (file._id.endsWith('.md')) {
       visit(file.head.image.src, (n) => {
-        file.head.image.src = `${'https://1712-2001-44c8-4560-e714-f5f4-589e-66ff-ef4b.ngrok-free.app'}${n}`
+        file.head.image.src = `${process.env.BASE_URL}/${n}`
       })
     }
   })
